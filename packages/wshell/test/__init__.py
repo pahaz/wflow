4fbd36a65b0ea7b0aa6fb9d46ffca834f3115556
pahaz <pahaz.blinov@gmail.com>
2015-05-28 03:38:00 +0500

    fix py2.7 compatability

--- packages/wshell/test/__init__.py
+++ packages/wshell/test/__init__.py
@@ -1,4 +1,4 @@
-from __future__ import unicode_literals, print_function, generators, division
+from __future__ import print_function, generators, division
 import unittest
 
 from wshell.abc_command import AbstractCommand
